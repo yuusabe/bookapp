d208ca99b22c1813c67df41fbbfdb8aa46d8367a
ymskntk <nayamasaki@pasona.tech>
2020-11-05 15:38:12 +0900

    no message

--- BookProject/resources/views/information_of_book.blade.php
+++ BookProject/resources/views/information_of_book.blade.php
@@ -1,38 +1,28 @@
-<?php $title = "DTGBOOK【書籍詳細】";?>
+<?php $title = "DTGBOOK【書籍詳細画面】";?>
 <?php $csspath = "css/information_of_book.css";?>
 
 @extends("common.header");
 
-
-    <main>
-        <div id="book_p">
-            <div id="book">
-                <img src="book_001.png" id="icon" alt="アイコン" width="135" height="130" />
-            </div>
-            
-            <div id="book">
-                <p>
-                  タイトル：RPAのはじめかた
-                </p>
-                <p>
-                  出版社：技術評論社
-                </p>
-                <p>
-                  貸出状況：貸出可
-                </p>
-            </div>
+<main>
+    <div id="book_p">
+        <div id="book">
+            <img src="book_001.png" id="icon" alt="アイコン" width="135" height="130" />
         </div>
-
-        <div id="button">
-            <button type="button" class="btn btn-outline-secondary"  onclick="location.href='http://localhost:8001/書籍一覧'">
-                一覧へ
-            </button>
-
-            <button type="button" class="btn btn-outline-secondary" onclick="location.href='http://localhost:8001/貸出'">
-              貸出手続きへ
-            </button>
+        
+        <div id="book">
+            <p>タイトル：RPAのはじめかた</p>
+            <p>出版社：技術評論社</p>
+            <p>貸出状況：貸出可</p>
         </div>
+    </div>
+    <div id="button">
+        <button type="button" class="btn btn-outline-secondary"  onclick="location.href='list_of_books.html'">
+            一覧へ
+        </button>
+        <button type="button" class="btn btn-outline-secondary" onclick="location.href='lend_book.html'">
+            貸出手続きへ
+        </button>
+    </div>
+ </main>
 
-    </main>
-
-@extends("common.footer");
+ @section("common.footer");
