495ec5948f971fbc8c3420b1d59e43b349ff4508
ymskntk <nayamasaki@pasona.tech>
2020-11-06 11:58:44 +0900

    画像パス

--- BookProject/resources/views/lend_check.blade.php
+++ BookProject/resources/views/lend_check.blade.php
@@ -8,7 +8,7 @@
     <div id="all">
         <div id="book_p">
             <div id="book">
-                <img src="book_001.png" id="icon" alt="アイコン" width="135" height="130" />
+                <img src="image/book_001.png" id="icon" alt="アイコン" width="135" height="130" />
             </div> 
             <div id="book">
                 <p>タイトル：RPAのはじめかた</p>
