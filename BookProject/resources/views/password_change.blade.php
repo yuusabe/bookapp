2cb105d994f3864b277f11cdcf268974e22b838c
ymskntk <nayamasaki@pasona.tech>
2020-11-06 14:42:19 +0900

    password変更画面

--- BookProject/resources/views/password_change.blade.php
+++ BookProject/resources/views/password_change.blade.php
@@ -1,12 +1,40 @@
-<?php $title = "DTGBOOK【パスワード変更画面】";?>
+<?php $title = "DTGBOOK【パスワード設定画面】";?>
 <?php $csspath = "css/password_change.css";?>
 
 @extends("common.header")
 @section('body')
 
 <main>
-    <p>Hi!</p>
-    <p>めんどくさいのでテキストボックスは後で作ります。</p>
+    <div id="oldpass">
+        <p>
+            <div id="text">
+                現在のパスワード：
+            </div>
+            <form>
+                <input type="password" name="old_pass" size="20" minlength="8" value="現在のパスワードを入力">
+            </form>
+        </p>
+    </div>
+    <div id="newpass">
+        <p>
+            <div id="text">
+                新しいパスワード：
+            </div>
+            <form>
+                <input type="password" name="new_pass" size="20" minlength="8" value="新しいパスワードを入力">
+            </form>
+        </p>
+    </div>
+    <div id="newpass">
+        <p>
+            <div id="text">
+                新しいパスワード：
+            </div>
+            <form>
+                <input type="password" name="new_pass" size="20" minlength="8" value="確認のためもう一度入力してください」">
+            </form>
+        </p>
+    </div>
     <div id="button">
         <button type="button" class="btn btn-outline-secondary" onclick="location.href='mypage.html'">
             キャンセル
