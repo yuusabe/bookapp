6faa09f0112d6ee45deb265959b713360998a897
ymskntk <nayamasaki@pasona.tech>
2020-11-12 14:27:29 +0900

    account_management

--- BookProject/resources/views/account_management.blade.php
+++ BookProject/resources/views/account_management.blade.php
@@ -5,28 +5,31 @@
 @section('body')
 
 <main>
-<form action="http://www.dtg-shosekikanri2020-test.tk/account_management_check" method="get">
+  <form action="http://www.dtg-shosekikanri2020-test.tk/account_management_check" method="get">
+    <div id="text">
+      <p>登録する内容を入力してください。</p>
+    </div>
     <div id=tb_p>
-      <div id="account" id="tb">
-        <div id="text">
+      <div>
+        <div id="tb">
           <p>アカウント名</p>
         </div>
         <input type="text" id="tbox" name="account_name" placeholder="DTG太郎">
       </div>
-      <div id="email" id="tb">
-        <div id="text">
+      <div>
+        <div id="tb">
           <p>メールアドレス</p>
         </div>
         <input type="email" id="tbox" name="address" placeholder="abc@example.com">
       </div>
-      <div id="password" id="tb">
-        <div id="text">
+      <div>
+        <div id="tb">
           <p>パスワード</p>
         </div>
         <input type="text" id="tbox" name="password" placeholder="password">
       </div>
       <div id="radio">
-        <div id="text">
+        <div id="tb">
           <p>管理者権限</p>
         </div>
           <input type="radio" name="accounttype" value="一般ユーザ">一般ユーザ
@@ -38,13 +41,12 @@
         <button type="button" class="btn btn-outline-secondary" onclick="location.href='http://www.dtg-shosekikanri2020-test.tk/account_management_check'">
           アカウント登録
         </button>
+      </div>
+      <div id="button">
         <button type="submit" class="btn btn-outline-secondary" >
           アカウント登録（実験用）
         </button>
       </div>
-    </div>
-    <div id="text">
-      <p>アカウント情報</p>
     </div>
     <div id="button_p">
       <div id="button">
