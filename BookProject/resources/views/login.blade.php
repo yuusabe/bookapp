0677e121f70e210e0c84d9ff3d770e60b1f4c03c
真島　皓太郎 <kmashima@pasona.tech>
2020-11-17 14:01:59 +0900

    login クッキー操作

--- BookProject/resources/views/login.blade.php
+++ BookProject/resources/views/login.blade.php
@@ -16,8 +16,34 @@
     <form action="https://www-cf.dtg-shosekikanri2020-test.tk/login_check" method="get">
       <input type="text" id="login" class="fadeIn second" name="email" placeholder="メールアドレス">
       <input type="text" id="password" class="fadeIn third" name="password" placeholder="パスワード">
-      <input type="submit" class="fadeIn fourth" value="ログイン" >
+      <input type="submit" class="fadeIn fourth" value="ログイン" onclick="writeCookie()" >
+      <button onclick="readCookie()">読み込み</button><br>
     </form>
+
+    <script type="text/javascript">
+      // cookieの値を読み書きする要素
+      var login = document.getElementById("login");
+      var password = document.getElementById("password");
+
+      // 書き込み
+      function writeCookie() {
+        var login_v = login.value;
+        var password_v = password.value;
+        document.cookie = "email=" + login_v;
+        document.cookie = "password=" + password_v;
+        console.log(document.cookie);
+      }
+
+      function readCookie() {
+        var tmp = document.cookie;
+        var login_v = document.cookie.replace(/(?:(?:^|.*;\s*)email\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        var password_v = document.cookie.replace(/(?:(?:^|.*;\s*)password\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        login.value = login_v;
+        password.value = password_v;
+        console.log(document.cookie);
+      }
+
+    </script>
 
     <!-- Remind Passowrd -->
     <div id="formFooter">
