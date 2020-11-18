139d236c54ac7bf4a2622413f9172de1c9ce836d
真島　皓太郎 <kmashima@pasona.tech>
2020-11-18 11:50:07 +0900

    ログインエラー対策

--- BookProject/resources/views/login.blade.php
+++ BookProject/resources/views/login.blade.php
@@ -15,20 +15,20 @@
     <?php
     function checkLogin()
     {
-      $email_in = $_COOKIE["email"];
-      $password_in = $_COOKIE["password"];
+      //$email_in = $_COOKIE["email"];
+      //$password_in = $_COOKIE["password"];
       $address = "login";
 
-      if ($email_in == "email@email" || $password_in == "password")
-      {
-          $comment = "ログインに成功しました!";
-          $address = "list_of_books";
-      }
-      else
-      {
-          $comment = "ログインに失敗しました";
-          $address = "login";
-      }
+      // if ($email_in == "email@email" || $password_in == "password")
+      // {
+      //     $comment = "ログインに成功しました!";
+      //     $address = "list_of_books";
+      // }
+      // else
+      // {
+      //     $comment = "ログインに失敗しました";
+      //     $address = "login";
+      // }
       echo $address;
     }
     ?>
