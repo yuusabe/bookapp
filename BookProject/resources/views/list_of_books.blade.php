d693e1858e062ac6f150fed784ad2d68f1e92b1d
yuusabe <yu21093261@gmail.com>
2020-11-27 21:20:49 +0900

    書籍一覧オブジェクトデータ取得実験

--- BookProject/resources/views/list_of_books.blade.php
+++ BookProject/resources/views/list_of_books.blade.php
@@ -43,7 +43,7 @@
   
   <input id="sbtn" id="search" type="submit" value="検索" /></div>
   </form> -->
-
+  {{$data->category_name[5]}}
   @foreach($data as $d)
   <div id="book_p">
     <div id="book">
@@ -81,7 +81,6 @@
       </button>
     </div>
   </div>
-  {{$d->multi}}
   @endforeach
 
   <div id="button_p">
