195d7923c61e1274e3b7cbfb3972a6762f5581a9
yuusabe <yu21093261@gmail.com>
2020-11-27 21:25:08 +0900

    書籍一覧オブジェクト表示

--- BookProject/resources/views/list_of_books.blade.php
+++ BookProject/resources/views/list_of_books.blade.php
@@ -43,7 +43,7 @@
   
   <input id="sbtn" id="search" type="submit" value="検索" /></div>
   </form> -->
-  {{$data->category_name[5]}}
+  {{$data->title[5]}}
   @foreach($data as $d)
   <div id="book_p">
     <div id="book">
