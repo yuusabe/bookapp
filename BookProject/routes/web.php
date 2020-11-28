6cc67cdb4f417554202225e7b1f35ff8eeae3df2
yuusabe <yu21093261@gmail.com>
2020-11-29 03:40:17 +0900

    tet

--- BookProject/routes/web.php
+++ BookProject/routes/web.php
@@ -131,7 +131,7 @@ Route::post('/testpage', 'App\Http\Controllers\TestController@i_post')->name("bo
 
 //書籍一覧画面からの遷移
 //書籍一覧画面表示
-Route::get('/list_of_books', 'App\Http\Controllers\GetbookController@getbook');
+Route::get('/list_of_books', 'App\Http\Controllers\TestController@getbook');
 //書籍一覧画面、各ボタン押下時
 Route::post('/list_of_books', 'App\Http\Controllers\TestController@i_post')->name("book.i_post");
 
